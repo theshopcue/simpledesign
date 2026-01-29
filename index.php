e80a4e8e3ea657836ca6fc3ecd3507b07b2ddb9d
theshopcue <theshopcue@gmail.com>
2026-01-29 12:53:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -56,6 +56,28 @@ footer{text-align:center;padding:40px;background:#111;color:#fff;margin-top:60px
 <script>
 function go(id){document.getElementById(id).scrollIntoView({behavior:'smooth'});}
 </script>
+
+
+
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69733b1558104b1978d41cb5/1jfl1tjkf';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+
+
+
+
+    
 </head>
 <body>
 
